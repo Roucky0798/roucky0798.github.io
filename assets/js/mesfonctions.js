d44959648016648578f3f4ed8ee8bb2d782bd68b
Roucky0798 <Roucky0798@gmail.com>
2017-11-20 16:28:51 +0100

    Site

--- assets/js/mesfonctions.js
+++ assets/js/mesfonctions.js
@@ -1,8 +1,6 @@
 ﻿$(document).ready(function(){
     // au clic sur un lien
     $('a').on('click', function(evt){
-       // bloquer le comportement par défaut: on ne rechargera pas la page
-       evt.preventDefault(); 
        // enregistre la valeur de l'attribut  href dans la variable target
 	var target = $(this).attr('href');
        /* le sélecteur $(html, body) permet de corriger un bug sur chrome 
